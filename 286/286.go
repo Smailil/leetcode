6d60115ea46a3c5fb167e407b97e414daa0b6885
Smailil <z20p007@gmail.com>
2026-02-02 20:13:27 +0300

    286

--- 286/286.go
+++ 286/286.go
@@ -28,6 +28,52 @@ rooms[i][j] имеет значение -1, 0 или 2^31 - 1.
 
 */
 
+type cell struct {
+	row int
+	col int
+}
+
 func islandsAndTreasure(grid [][]int) {
-    
+    rows, cols := len(grid), len(grid[0])
+	visited := make(map[cell]struct{})
+	queue := []cell{}
+
+	addCell := func(r, c int) {
+		if (r < 0 || r == rows || c < 0 || c == cols) {
+			return
+		}
+		room := cell{r, c}
+		if _, ok := visited[room]; ok {
+			return
+		}
+		if grid[r][c] == -1 {
+			return
+		}
+		visited[room] = struct{}{}
+		queue = append(queue, room)
+	}
+
+	for r := range(rows) {
+		for c := range(cols) {
+			if grid[r][c] == 0 {
+				treasure := cell{r, c}
+				visited[treasure] = struct{}{}
+				queue = append(queue, treasure)
+			}
+		}
+	}
+	distance := 0
+	for len(queue) > 0 {
+		levelSize := len(queue)
+		for range(levelSize) {
+			current := queue[0]
+			queue = queue[1:]
+			grid[current.row][current.col] = distance
+			addCell(current.row+1, current.col)
+			addCell(current.row-1, current.col)
+			addCell(current.row, current.col+1)
+			addCell(current.row, current.col-1)
+		}
+		distance++
+	}
 }
